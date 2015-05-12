9029bad93f2eaa39d26f1316ff0bb4a73ea3052e
Matt Gaspar <mgaspar@godaddy.com>
2015-05-11 23:51:48 -0700

    Add pushd and wget commands

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# To run this curl -sSL https://github.com/thomcost/cgm-remote-monitor/blob/master/install.sh | bash
+pushd /etc/puppet/modules
+wget https://raw.githubusercontent.com/thomcost/cgm-remote-monitor/master/install.pp -O /etc/puppet/modules/install.pp
 puppet module install puppetlabs-vcsrepo
 puppet apply --verbose --show_diff -e "class { 'cgm_remote_monitor': }"
+popd
